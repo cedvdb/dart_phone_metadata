93b9b6952282dfd4ac9f0f0f50e4719b17adbb0d
cedvdb <cedvandenbosch@gmail.com>
2021-07-20 18:07:39 +0200

    export

--- lib/phone_number_metadata.dart
+++ lib/phone_number_metadata.dart
@@ -2,3 +2,4 @@ export 'src/generated/dial_code_to_iso_code.dart';
 export 'src/generated/light_metadata_by_iso_code.dart';
 export 'src/generated/extended_metadata_by_iso_code.dart';
 export 'src/models/phone_metadata_extended.dart';
+export 'src/models/phone_metadata.dart';
