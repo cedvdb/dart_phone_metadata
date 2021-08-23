ac277bb80ec7ccbc578bcfc19cbc89bad4fe8796
cedvdb <cedvandenbosch@gmail.com>
2021-08-23 02:21:52 +0200

    export lengths and patterns

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 2.0.1
+
+- Correctly export pattern and lengths
+
 ## 2.0.0
 
 - Split metadata into metadata, patterns and lengths
--- lib/phone_number_metadata.dart
+++ lib/phone_number_metadata.dart
@@ -1,4 +1,8 @@
 export 'src/generated/dial_code_to_iso_code.dart';
+export 'src/generated/metadata_by_iso_code.dart';
+export 'src/generated/metadata_lengths_by_iso_code.dart';
+export 'src/generated/metadata_patterns_by_iso_code.dart';
 
-export 'src/models/phone_metadata_patterns.dart';
 export 'src/models/phone_metadata.dart';
+export 'src/models/phone_metadata_patterns.dart';
+export 'src/models/phone_metadata_lengths.dart';
