50039067ef5e9c73b7a7cd1f6ba7345477c7974c
bitsdominicana <andy.melo@bits.net.do>
2022-08-30 13:54:07 -0400

    added ISo.Do format

--- lib/src/generated/metadata_formats_by_iso_code.dart
+++ lib/src/generated/metadata_formats_by_iso_code.dart
@@ -1431,7 +1431,7 @@ const metadataFormatsByIsoCode = <IsoCode, PhoneMetadataFormats>{
       pattern: r'(\d{3})(\d{3})(\d{2})(\d{2})',
       nationalPrefixFormattingRule: null,
       leadingDigits: ['310'],
-      format: r'$1-$2-$3-$4',
+      format: r'$1-$2-$3$4',
       intlFormat: null,
     )
   ],
